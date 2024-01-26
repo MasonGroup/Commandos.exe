8e3b8612c51fce8c57f9a79572926a67a2fa0bf8
Freemasonry <95870255+MasonGroup@users.noreply.github.com>
2024-01-26 22:08:22 +0300

    Add files via upload

---
+++ Commandos.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Windows.Forms;
+
+class Program
+{
+    static void Main()
+    {
+        MessageBox.Show("Gangsta freemasonry made me do it", "Commandos");
+        Environment.Exit(0);
+    }
+}
